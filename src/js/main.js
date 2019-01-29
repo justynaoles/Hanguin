3dde84d6550a5bf781638e29a502a0260b819911
Justyna Oleś <justynaoles88@gmail.com>
2019-01-29 14:06:39 +0100

    update correct array

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
 
+
     function startGame(data) {
 
         const word = data.toUpperCase();
@@ -9,18 +10,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const password = document.querySelector('.password');
         let correctArray = [];
         let errorsArray =[];
+        let hintsArray =[];
+        let restWord =[];
         let missedChances=0;
         const iceberg = document.querySelectorAll('.ice');
         const quack = new Audio('src/audio/quack.wav');
+        const win = new Audio('src/audio/win.flac');
+        const lose = new Audio('src/audio/lose.wav');
+        const error = new Audio('src/audio/error.wav');
+        const repeat = new Audio('src/audio/repeat.wav');
         let gameOver = document.createElement('div');
         gameOver.classList.add('end-game');
-        gameOver.innerHTML=" <div class='end-box'><div class='end-box-wrapper'><img src='src/img/gameover.PNG'><div class='end-box-play'><h4>You melt iceberg!</h4><button class='btn'>Play again</button></div></div><div class='water'></div></div>";
+        gameOver.innerHTML=" <div class='end-box'><div class='end-box-wrapper'><img src='src/img/gameover.PNG'><div class='end-box-play'><h4 class='end-box-header'>You melt iceberg!</h4><button class='btn'>Play again</button></div></div><div class='water'></div></div>";
         let gameWin = document.createElement('div');
         gameWin.classList.add('end-game');
-        gameWin.innerHTML= "<div class='end-box'><div class='end-box-wrapper'><img src='src/img/win.PNG'><div class='end-box-play'><h4>You saved cute pinguin!</h4><button class='btn'>Play again</button></div></div></div></div>"
+        gameWin.innerHTML= "<div class='end-box'><div class='end-box-wrapper'><img src='src/img/win.PNG'><div class='end-box-play'><h4 class='end-box-header'>You saved cute pinguin!</h4><button class='btn'>Play again</button></div></div></div></div>"
         const container = document.querySelector('.container');
-            
-        
+       
+
+
         console.log(word);
         
         function hideWord(){
@@ -30,16 +38,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 if(i%3 == 0) {
         
                 hiddenWord = hiddenWord + word[i];
+                hintsArray.push(word[i]);
+                
             
                 }
         
                 else {
         
                 hiddenWord = hiddenWord + " ";
+                restWord.push(word[i]);
                 }
             }
-        
-        
+      
         }
         
         hideWord();
@@ -67,44 +77,77 @@ document.addEventListener('DOMContentLoaded', (event) => {
         
         displayHiddenWord();
 
-      
-        
+
+        // dodanie do "correct" podpowiedzi
+
+            for (let i = 0; i<restWord.length; i++) {     
+    
+                if (restWord.indexOf(hintsArray[i]) === -1) {
+    
+                        correctArray.push(hintsArray[i]);
+                    }
+                 }
+            
         
         document.addEventListener('keydown', guess);
+        $(document).on("click",function(){
+
+            $('.dummy').focus();
+
+        });
+        $('.dummy').focus();
+    
 
         
         function guess(e) {
 
           resetAnimation();
-        //   alert('klik');
 
-            let key = e.keyCode;
-            let keyChart = String.fromCharCode(key).toUpperCase();
+            let key;
             let correctAnswer = false;
+            let includedInHints = false;
+
+
+            if (e.keyCode == 229)  {
+
+                key =$('.dummy').val().slice( $('.dummy').val().length-1,$('.dummy').val().length ).toUpperCase().charCodeAt(0)
+            }
+        
+            else  {
+        
+                key = e.keyCode;
+            }
+             
 
     
 
         
             if ((key >= 65 && key <= 90) || (key >= 97 && key <= 122))      {
 
-            
+                let keyChart = String.fromCharCode(key).toUpperCase();
                 
         
                for (let i = 0; i<word.length; i++) {
         
                     if (word.charAt(i) == keyChart ) {
         
-                        hiddenWord= hiddenWord.showLetter(i,keyChart);
+                        hiddenWord = hiddenWord.showLetter(i,keyChart);
                 
                         correctAnswer = true;
                     }
+
+                    if (hintsArray.indexOf(keyChart) != -1) {
+
+                        includedInHints = true;
+                    }
                }
+
+               console.log(includedInHints);
         
         
                 if (correctAnswer) {
 
-                   
-        
+
                     checkCorrectArray(correctArray, keyChart);
                     displayHiddenWord();
         
@@ -112,7 +155,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         
                 else {
 
-                   
         
                     checkErrorArray(errorsArray, keyChart);
                 }
@@ -126,10 +168,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
         
             if (hiddenWord==word) {
 
-           
-                console.log('victory');
                 document.removeEventListener('keypress', guess);
+                win.play();
                 document.body.insertBefore(gameWin,container);
+             
 
                 setTimeout(function(){ 
                         
@@ -147,6 +189,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             if (missedChances > iceberg.length -1 ) {
                 console.log('przegrales');
                 document.removeEventListener('keypress', guess);
+                lose.play();
                 document.body.insertBefore(gameOver,container);
                 setTimeout(function(){
 
@@ -158,12 +201,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
         
         }
         
+            function checkCorrectArray(array, item) {
 
-            function checkCorrectArray(array,item) {
-
-    
-        
-                if(array.indexOf(item) < 0) {
+               
+                    
+                if( array.indexOf(item) <0) {
 
                     array.push(item);
 
@@ -176,13 +218,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     
                     }, 100);
                 }
-        
+
                 else {
-                    console.log("mamy to już w tablicy");
+                    repeat.play();
                 }
 
-        
+
             }
+
+
+
+
         
             function checkErrorArray(array,item) {
         
@@ -190,6 +236,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 if(array.indexOf(item) < 0) {
         
                     array.push(item);
+                    error.play();
                     missedChances++;
                     document.querySelector('.level'+missedChances).classList.add('show');
                     document.querySelector('.iceberg-piece'+missedChances).classList.add('hide');
@@ -199,7 +246,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 }
         
                 else {
-                    console.log("mamy to już w tablicy");
+                    repeat.play();
                 }
         
         
@@ -240,16 +287,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
         
         }
 
-
+     
+    
+      
         
         
         function readData() {
-            const apiURL = 'https://api.datamuse.com/words?rel_jjb=ocean&fbclid=IwAR26oGUUf8wnHiz-tw8yeUUJaK_-FcQ_2imW5fhcFTDLiVjqAhe8iIoJ1Gk';
+            // const apiURL = 'https://api.datamuse.com/words?rel_jjb=ocean&fbclid=IwAR26oGUUf8wnHiz-tw8yeUUJaK_-FcQ_2imW5fhcFTDLiVjqAhe8iIoJ1Gk';
+
+
+            const apiURL = 'src/json/words.json';
 
          
         
             $.ajax({
                 
+                type: 'GET',
                 url: apiURL,
                 dataType: 'json'
             })
@@ -269,5 +322,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 });
+
 
 
